0ad6347ec4aed8e0f562d06444fcceee37e51343
Ajith Raj <ajith.raj@intel.com>
2025-01-10 12:03:41 +0530

    Updated changes to test for gramine-direct

--- test_workloads.py
+++ test_workloads.py
@@ -19,7 +19,15 @@ ra_type = os.environ.get("RA_TYPE", "none")
 
 class Test_Workload_Results():
     @pytest.mark.examples
-    def test_mariadb_workload(self):
+    @pytest.mark.skipif(int(no_cores) < 16)
+    def test_mariadb_workload_gramine_sgx(self):
         # Test Sequence - Spawn mariadb server in background, run mariadb client, print SUCCESS if successfully launched
         # Check if the string "SUCCESS" is present in and client_output which generated after running the Makefile
-        assert "SUCCESS" in open("CI-Examples/mariadb/client_output", "r").read()
+        assert "SUCCESS" in open("CI-Examples/mariadb/client_output_gramine-sgx", "r").read()
+    
+    @pytest.mark.examples
+    @pytest.mark.skipif(int(no_cores) < 16)
+    def test_mariadb_workload_gramine_direct(self):
+        # Test Sequence - Spawn mariadb server in background, run mariadb client, print SUCCESS if successfully launched
+        # Check if the string "SUCCESS" is present in and client_output which generated after running the Makefile
+        assert "SUCCESS" in open("CI-Examples/mariadb/client_output_gramine-direct", "r").read()
