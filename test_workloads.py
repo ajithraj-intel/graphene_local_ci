5e111c8a2ef6e3a3defbfcb39d67a367c2a22019
Ajith Raj <ajith.raj@intel.com>
2025-01-15 17:37:40 +0530

    added changes to run direct tests

--- test_workloads.py
+++ test_workloads.py
@@ -19,7 +19,7 @@ ra_type = os.environ.get("RA_TYPE", "none")
 
 class Test_Workload_Results():
     @pytest.mark.examples
-    @pytest.mark.skipif(int(no_cores) < 16, reason="Run only on servers, not on client machines, due to memory problems")
+    @pytest.mark.skipif(int(no_cores) < 16 and sgx_mode == '1', reason="Run only on servers, not on client machines, due to memory problems")
     def test_mariadb_workload_gramine_sgx(self):
         # Test Sequence - Spawn mariadb server in background, run mariadb client, print SUCCESS if successfully launched
         # Check if the string "SUCCESS" is present in and client_output which generated after running the Makefile
