0b6381a195480f17ed6b0ee2701c6f34dc084d12
Ajith Raj <ajith.raj@intel.com>
2025-01-16 10:14:59 +0530

    CHanged or -> and

--- test_workloads.py
+++ test_workloads.py
@@ -19,7 +19,7 @@ ra_type = os.environ.get("RA_TYPE", "none")
 
 class Test_Workload_Results():
     @pytest.mark.examples
-    @pytest.mark.skipif(not(int(no_cores) > 16 or sgx_mode == '1'), reason="Run only on server machines, when dDirect is disabled")
+    @pytest.mark.skipif(not(int(no_cores) > 16 and sgx_mode == '1'), reason="Run only on server machines, when dDirect is disabled")
     def test_mariadb_workload_gramine_sgx(self):
         # Not is added in the skip condition to improve readability
         # Test Sequence - Spawn mariadb server in background, run mariadb client, print SUCCESS if successfully launched
@@ -29,7 +29,7 @@ class Test_Workload_Results():
     @pytest.mark.examples
     @pytest.mark.skipif(not(int(no_cores) > 16 and sgx_mode == '0'), reason="Run only on server machines, when dDirect is enabled")
     def test_mariadb_workload_gramine_direct(self):
-        # Not is added in the skip condition to improve readability
+        
         # Test Sequence - Spawn mariadb server in background, run mariadb client, print SUCCESS if successfully launched
         # Check if the string "SUCCESS" is present in and client_output which generated after running the Makefile
         assert "SUCCESS" in open("CI-Examples/mariadb/client_output_gramine-direct", "r").read()
